e0d3f055df3299e1fb4e7e895b01dccfe43c0ddf
unknown <aristosmiliaresis@gmail.com>
2023-11-10 11:14:15 +0200

    .

--- wordlist_builder.sh
+++ wordlist_builder.sh
@@ -6,29 +6,38 @@ then
 	exit 1
 fi
 
-# TO LOOK INTO:
-#https://github.com/maverickNerd/wordlists/blob/master/SecListsCurated/quickhits.txt
-#https://github.com/danielmiessler/SecLists/blob/master/Discovery/Web-Content/aem2.txt
-#https://github.com/danielmiessler/SecLists/tree/master/Discovery/Web-Content/CMS
-#https://github.com/danielmiessler/SecLists/tree/master/Discovery/Web-Content/WebTechnologyPaths-Trickest-Wordlists
-
 out_dir=$1
+mkdir -p $out_dir/tech/
+cd $out_dir
+out_dir=`pwd`
+
 tmp_dir=`mktemp -d`
 trap "rm -rf $tmp_dir" EXIT
 
 cd $tmp_dir
-mkdir -p $out_dir/tech/
 
 git clone https://github.com/danielmiessler/SecLists.git
 git clone https://github.com/assetnote/commonspeak2-wordlists
 wget -r --no-parent -R "index.html*" https://wordlists-cdn.assetnote.io/data/ -nH
 
+general=(
+	"https://raw.githubusercontent.com/danielmiessler/RobotsDisallowed/master/top10000.txt"
+	"https://wordlists-cdn.assetnote.io/data/manual/raft-medium-files.txt"
+	"https://raw.githubusercontent.com/six2dez/OneListForAll/main/onelistforallshort.txt"
+	"https://raw.githubusercontent.com/six2dez/OneListForAll/main/dict/wellknown_short.txt"
+	"https://raw.githubusercontent.com/six2dez/OneListForAll/main/dict/ws-dirs_short.txt"
+	"https://raw.githubusercontent.com/six2dez/OneListForAll/main/dict/web-files_short.txt"
+	"https://raw.githubusercontent.com/six2dez/OneListForAll/main/dict/common-api-endpoints_short.txt"
+	"https://raw.githubusercontent.com/six2dez/OneListForAll/main/dict/graphql_short.txt"
+	"https://raw.githubusercontent.com/six2dez/OneListForAll/main/dict/jar_short.txt"
+	"https://raw.githubusercontent.com/six2dez/OneListForAll/main/dict/nginx_short.txt"
+	"https://raw.githubusercontent.com/six2dez/OneListForAll/main/dict/swagger_short.txt"
+)
+
 high_impact_lists=( 
 	"https://raw.githubusercontent.com/danielmiessler/SecLists/master/Discovery/Web-Content/UnixDotfiles.fuzz.txt"
 	"https://wordlists-cdn.assetnote.io/data/manual/bak.txt"
 	"https://raw.githubusercontent.com/danielmiessler/SecLists/master/Discovery/Web-Content/Common-DB-Backups.txt"
-	"https://raw.githubusercontent.com/aristosMiliaressis/wordlist_builder/master/temp/high-impact-files.txt"
-
 	"https://raw.githubusercontent.com/danielmiessler/SecLists/master/Discovery/Web-Content/hashicorp-vault.txt"
 	"https://raw.githubusercontent.com/danielmiessler/SecLists/master/Discovery/Web-Content/hashicorp-consul-api.txt"
 	"https://raw.githubusercontent.com/danielmiessler/SecLists/master/Discovery/Web-Content/CommonBackdoors-ASP.fuzz.txt"
@@ -36,7 +45,24 @@ high_impact_lists=(
 	"https://raw.githubusercontent.com/danielmiessler/SecLists/master/Discovery/Web-Content/CommonBackdoors-PHP.fuzz.txt"
 	"https://raw.githubusercontent.com/danielmiessler/SecLists/master/Discovery/Web-Content/CommonBackdoors-PL.fuzz.txt"
 	"https://raw.githubusercontent.com/danielmiessler/SecLists/master/Discovery/Web-Content/vulnerability-scan_j2ee-websites_WEB-INF.txt"
+	"https://raw.githubusercontent.com/aristosMiliaressis/wordlist_builder/master/temp/high-impact-files.txt"
 	"https://raw.githubusercontent.com/aristosMiliaressis/wordlist_builder/master/temp/high-impact-endpoints.txt"
+	"https://raw.githubusercontent.com/six2dez/OneListForAll/main/dict/zip_long.txt"
+	"https://raw.githubusercontent.com/six2dez/OneListForAll/main/dict/sql_long.txt"
+	"https://raw.githubusercontent.com/six2dez/OneListForAll/main/dict/rar_long.txt"
+	"https://raw.githubusercontent.com/six2dez/OneListForAll/main/dict/env_short.txt"
+	"https://raw.githubusercontent.com/six2dez/OneListForAll/main/dict/common-db-backups_short.txt"
+	"https://raw.githubusercontent.com/six2dez/OneListForAll/main/dict/conf_long.txt"
+	"https://raw.githubusercontent.com/six2dez/OneListForAll/main/dict/config_long.txt"
+	"https://raw.githubusercontent.com/six2dez/OneListForAll/main/dict/git_config_short.txt"
+	"https://raw.githubusercontent.com/six2dez/OneListForAll/main/dict/htaccess_short.txt"
+	"https://raw.githubusercontent.com/six2dez/OneListForAll/main/dict/ini_long.txt"
+	"https://raw.githubusercontent.com/six2dez/OneListForAll/main/dict/k8s_short.txt"
+	"https://raw.githubusercontent.com/six2dez/OneListForAll/main/dict/mdb_short.txt"
+	"https://raw.githubusercontent.com/six2dez/OneListForAll/main/dict/mdf_short.txt"
+	"https://raw.githubusercontent.com/six2dez/OneListForAll/main/dict/reverse-proxy-inconsistencies_short.txt"
+	"https://raw.githubusercontent.com/six2dez/OneListForAll/main/dict/proxy-conf_short.txt"
+	"https://raw.githubusercontent.com/six2dez/OneListForAll/main/dict/debug_short.txt"
 )
 
 file_lists=(
@@ -45,6 +71,10 @@ file_lists=(
 	"https://wordlists-cdn.assetnote.io/data/automated/httparchive_xml_2023_06_28.txt"
 	"https://wordlists-cdn.assetnote.io/data/automated/httparchive_txt_2023_06_28.txt"
 	"https://wordlists-cdn.assetnote.io/data/automated/httparchive_html_htm_2023_06_28.txt"
+	"https://raw.githubusercontent.com/six2dez/OneListForAll/main/dict/yaml_long.txt"
+	"https://raw.githubusercontent.com/six2dez/OneListForAll/main/dict/yml_long.txt"
+	"https://raw.githubusercontent.com/six2dez/OneListForAll/main/dict/txt_long.txt"
+	"https://raw.githubusercontent.com/six2dez/OneListForAll/main/dict/tpl_short.txt"
 )
 
 directory_lists=(
@@ -61,6 +91,18 @@ iis_asp_wordlists=(
 	"https://raw.githubusercontent.com/danielmiessler/SecLists/master/Discovery/Web-Content/IIS.fuzz.txt"
 	"https://raw.githubusercontent.com/danielmiessler/SecLists/master/Discovery/Web-Content/iis-systemweb.txt"
 	"https://raw.githubusercontent.com/SooLFaa/fuzzing/master/webserver/iis.txt"
+	"https://raw.githubusercontent.com/six2dez/OneListForAll/main/dict/windows-asp_short.txt"
+	"https://raw.githubusercontent.com/six2dez/OneListForAll/main/dict/windows-aspx_short.txt"
+	"https://raw.githubusercontent.com/six2dez/OneListForAll/main/dict/wsdl_short.txt"
+	"https://raw.githubusercontent.com/six2dez/OneListForAll/main/dict/webconfig_short.txt"
+	"https://raw.githubusercontent.com/six2dez/OneListForAll/main/dict/svc_short.txt"
+	"https://raw.githubusercontent.com/six2dez/OneListForAll/main/dict/sln_short.txt"
+	"https://raw.githubusercontent.com/six2dez/OneListForAll/main/dict/csproj_short.txt"
+	"https://raw.githubusercontent.com/six2dez/OneListForAll/main/dict/pdb_long.txt"
+	"https://raw.githubusercontent.com/six2dez/OneListForAll/main/dict/ashx_short.txt"
+	"https://raw.githubusercontent.com/six2dez/OneListForAll/main/dict/asmx_short.txt"
+	"https://raw.githubusercontent.com/six2dez/OneListForAll/main/dict/ascx_short.txt"
+	"https://raw.githubusercontent.com/six2dez/OneListForAll/main/dict/asax_short.txt"
 )
 tomcat_jsp_wordlists=(
 	"https://wordlists-cdn.assetnote.io/data/automated/httparchive_jsp_jspa_do_action_2023_06_28.txt"
@@ -69,6 +111,8 @@ tomcat_jsp_wordlists=(
 	"https://raw.githubusercontent.com/danielmiessler/SecLists/master/Discovery/Web-Content/tomcat.txt"
 	"https://raw.githubusercontent.com/danielmiessler/SecLists/master/Discovery/Web-Content/ApacheTomcat.fuzz.txt"
 	"https://raw.githubusercontent.com/danielmiessler/SecLists/master/Discovery/Web-Content/WebTechnologyPaths-Trickest-Wordlists/tomcat-all-levels.txt"
+	"https://raw.githubusercontent.com/aristosMiliaressis/wordlist_builder/master/temp/tomcat-examples.txt"
+	"https://raw.githubusercontent.com/six2dez/OneListForAll/main/dict/springboot_short.txt"
 )
 apache_php_wordlists=(
 	"https://wordlists-cdn.assetnote.io/data/automated/httparchive_php_2023_06_28.txt"
@@ -77,16 +121,33 @@ apache_php_wordlists=(
 	"https://wordlists-cdn.assetnote.io/data/manual/pl.txt"
 	"https://raw.githubusercontent.com/danielmiessler/SecLists/master/Discovery/Web-Content/Apache.fuzz.txt"
 	"https://raw.githubusercontent.com/danielmiessler/SecLists/master/Discovery/Web-Content/apache.txt"
+	"https://raw.githubusercontent.com/six2dez/OneListForAll/main/dict/symphony_long.txt"
+	"https://raw.githubusercontent.com/six2dez/OneListForAll/main/dict/drupal_long.txt"
+)
+django_wordlists=(
+	"https://raw.githubusercontent.com/six2dez/OneListForAll/main/dict/django_long.txt"
+	"https://raw.githubusercontent.com/six2dez/OneListForAll/main/dict/py_short.txt"
+)
+flask_wordlists=(
+	"https://raw.githubusercontent.com/six2dez/OneListForAll/main/dict/py_short.txt"
 )
 nginx_wordlists=(
 	"https://raw.githubusercontent.com/danielmiessler/SecLists/master/Discovery/Web-Content/nginx.txt"
+	"https://raw.githubusercontent.com/six2dez/OneListForAll/main/dict/nginx_short.txt"
+	"https://raw.githubusercontent.com/six2dez/OneListForAll/main/dict/swagger_short.txt"
+	"https://raw.githubusercontent.com/six2dez/OneListForAll/main/dict/reverse-proxy-inconsistencies_short.txt"
+	"https://raw.githubusercontent.com/six2dez/OneListForAll/main/dict/proxy-conf_short.txt"
 )
 rails_wordlists=(
 	"https://raw.githubusercontent.com/danielmiessler/SecLists/master/Discovery/Web-Content/ror.txt"
+	"https://raw.githubusercontent.com/six2dez/OneListForAll/main/dict/ruby_rails_short.txt"
+	"https://raw.githubusercontent.com/six2dez/OneListForAll/main/dict/ror_short.txt"
+	"https://raw.githubusercontent.com/six2dez/OneListForAll/main/dict/rails_short.txt"
 )
 
 api_wordlists=(
 	"https://wordlists-cdn.assetnote.io/data/automated/httparchive_apiroutes_2023_06_28.txt"
+	"https://raw.githubusercontent.com/danielmiessler/SecLists/master/Discovery/Web-Content/api/api-endpoints.txt"
 )
 
 read_from_stdin() {
@@ -101,6 +162,7 @@ normalize_prefix() {
 }
 
 filter_junk() {
+	LC_ALL='en_US.UTF-8'
 	read_from_stdin \
 		| rev | cut -d '/' -f 1 | cut -d '\' -f 1 | rev \
 		| grep -E '[A-Za-z0-9]' \
@@ -117,7 +179,7 @@ filter_junk() {
 		| grep -v '>>' \
 		| grep -v '::' \
 		| filter_recursive_deadends \
-		| sort -u
+		| awk '!x[$0]++'
 }
 
 filter_files() {
@@ -142,7 +204,7 @@ filter_recursive_deadends() {
 
 filter_duplicates() {
 	temp=`mktemp`
-	cat $1 | sort -u > $temp
+	cat $1 | awk '!x[$0]++' > $temp
 	cat $temp > $1
 	rm $temp
 }
@@ -153,116 +215,93 @@ deduplicate() {
 	read_from_stdin | anew $temp
 }
 
-for list in "${high_impact_lists[@]}"
-do
-	temp=`mktemp`
-	curl -s $list -o $temp
-	cat $temp | anew $out_dir/high_impact.txt >/dev/null
-	rm $temp
-done
+export -f read_from_stdin
+export -f normalize_prefix
+export -f filter_junk
+export -f filter_files
+export -f grep_high_impact_extensions
+export -f grep_all_extensions
+export -f filter_recursive_deadends
+export -f filter_duplicates
+export -f deduplicate
+
+echo "${high_impact_lists[@]}" \
+	| tr  ' ' '\n' \
+	| parallel -j+0 "curl -s {} | anew -q $out_dir/high_impact.txt"
 
 grep_high_impact_extensions | anew $out_dir/high_impact.txt >/dev/null
 filter_duplicates $out_dir/high_impact.txt
 
-for list in "${file_lists[@]}"
-do
-	temp=`mktemp`
-	curl -s $list -o $temp
-	cat $temp | normalize_prefix | filter_junk | anew $out_dir/large_files.txt >/dev/null
-	rm $temp
-done
+echo "${file_lists[@]}" \
+	| tr  ' ' '\n' \
+	| parallel -j+0 "curl -s {} | normalize_prefix | filter_junk | anew -q $out_dir/large_files.txt"
+
 grep_all_extensions | anew $out_dir/large_files.txt >/dev/null
 filter_duplicates $out_dir/large_files.txt
 cat $out_dir/large_files.txt | deduplicate $out_dir/high_impact.txt > $out_dir/dedublicated_large_files.txt
 
-for list in "${directory_lists[@]}"
-do
-	temp=`mktemp`
-	curl -s $list -o $temp
-	cat $temp | normalize_prefix | filter_junk | filter_recursive_deadends | filter_files | anew $out_dir/directories.txt >/dev/null
-	rm $temp
-done
+
+echo "${directory_lists[@]}" \
+	| tr  ' ' '\n' \
+	| parallel -j+0 "curl -s {} | normalize_prefix | filter_junk | filter_recursive_deadends | filter_files | anew -q $out_dir/directories.txt"
+
 filter_duplicates $out_dir/directories.txt
 
-for list in "${iis_asp_wordlists[@]}"
-do
-	temp=`mktemp`
-	curl -s $list -o $temp
-	cat $temp | normalize_prefix | filter_junk | anew $out_dir/tech/iis_asp.txt >/dev/null
-	rm $temp
-done
+echo "${iis_asp_wordlists[@]}" \
+	| tr  ' ' '\n' \
+	| parallel -j+0 "curl -s {} | normalize_prefix | filter_junk | anew -q $out_dir/tech/iis_asp.txt"
 
-grep -a -hirE '\.(asp|aspx|ashx|asmx|wsdl|wadl|axd|asax)$' \
+ grep -a -hirE '\.(asp|aspx|ashx|asmx|wsdl|wadl|axd|asax)$' \
 	| filter_junk >>  $out_dir/tech/iis_asp.txt
 filter_duplicates $out_dir/tech/iis_asp.txt
 
-for list in "${tomcat_jsp_wordlists[@]}"
-do
-	temp=`mktemp`
-	curl -s $list -o $temp
-	cat $temp | normalize_prefix | filter_junk | anew $out_dir/tech/tomcat_jsp.txt >/dev/null
-	rm $temp
-done
+echo "${tomcat_jsp_wordlists[@]}" \
+	| tr  ' ' '\n' \
+	| parallel -j+0 "curl -s {} | normalize_prefix | filter_junk | anew -q $out_dir/tech/tomcat_jsp.txt"
 
 grep -a -hirE '\.(jsp|jspa|do|action)$' \
 	| filter_junk  >>  $out_dir/tech/tomcat_jsp.txt
 filter_duplicates $out_dir/tech/tomcat_jsp.txt
 
-for list in "${apache_php_wordlists[@]}"
-do
-	temp=`mktemp`
-	curl -s $list -o $temp
-	cat $temp | normalize_prefix | filter_junk | anew $out_dir/tech/apache_php.txt >/dev/null
-	rm $temp
-done
+echo "${apache_php_wordlists[@]}" \
+	| tr  ' ' '\n' \
+	| parallel -j+0 "curl -s {} | normalize_prefix | filter_junk | anew -q $out_dir/tech/apache_php.txt"
 
 grep -a -hirE '\.(php|cgi)$' \
 	| filter_junk  >>  $out_dir/tech/apache_php.txt
 filter_duplicates $out_dir/tech/apache_php.txt
 
-for list in "${nginx_wordlists[@]}"
-do
-	temp=`mktemp`
-	curl -s $list -o $temp
-	cat $temp | normalize_prefix | filter_junk | anew $out_dir/tech/nginx.txt >/dev/null
-	rm $temp
-done
+echo "${nginx_wordlists[@]}" \
+	| tr  ' ' '\n' \
+	| parallel -j+0 "curl -s {} | normalize_prefix | filter_junk | anew -q $out_dir/tech/nginx.txt"
 filter_duplicates $out_dir/tech/nginx.txt
 
-# for list in "${django_wordlists[@]}"
-# do
-	# temp=`mktemp`
-	# curl -s $list -o $temp
-	# cat $temp | normalize_prefix | filter_junk | anew $out_dir/tech/django.txt >/dev/null
-	# rm $temp
-# done
-# filter_duplicates $out_dir/tech/django.txt
+echo "${django_wordlists[@]}" \
+	| tr  ' ' '\n' \
+	| parallel -j+0 "curl -s {} | normalize_prefix | filter_junk | anew -q $out_dir/tech/django.txt"
+filter_duplicates $out_dir/tech/django.txt
 
-# for list in "${flask_wordlists[@]}"
-# do
-	# temp=`mktemp`
-	# curl -s $list -o $temp
-	# cat $temp | normalize_prefix | filter_junk | anew $out_dir/tech/flask.txt >/dev/null
-	# rm $temp
-# done
-# filter_duplicates $out_dir/tech/flask.txt
+# # for list in "${flask_wordlists[@]}"
+# # do
+	# # temp=`mktemp`
+	# # curl -s $list -o $temp
+	# # cat $temp | normalize_prefix | filter_junk | anew $out_dir/tech/flask.txt >/dev/null
+	# # rm $temp
+# # done
+# # filter_duplicates $out_dir/tech/flask.txt
 
-# for list in "${express_wordlists[@]}"
-# do
-	# temp=`mktemp`
-	# curl -s $list -o $temp
-	# cat $temp | normalize_prefix | filter_junk | anew $out_dir/tech/express.txt >/dev/null
-	# rm $temp
-# done
-# filter_duplicates $out_dir/tech/express.txt
+# # for list in "${express_wordlists[@]}"
+# # do
+	# # temp=`mktemp`
+	# # curl -s $list -o $temp
+	# # cat $temp | normalize_prefix | filter_junk | anew $out_dir/tech/express.txt >/dev/null
+	# # rm $temp
+# # done
+# # filter_duplicates $out_dir/tech/express.txt
 
-for list in "${rails_wordlists[@]}"
-do
-	temp=`mktemp`
-	curl -s $list -o $temp
-	cat $temp | normalize_prefix | filter_junk | anew $out_dir/tech/rails.txt >/dev/null
-	rm $temp
-done
+echo "${rails_wordlists[@]}" \
+	| tr  ' ' '\n' \
+	| parallel -j+0 "curl -s {} | normalize_prefix | filter_junk | anew -q $out_dir/tech/rails.txt"
 filter_duplicates $out_dir/tech/rails.txt
 
 cd - >/dev/null
